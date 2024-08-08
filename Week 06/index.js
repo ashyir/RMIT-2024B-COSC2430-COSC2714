8f3f175a88853ccf6a4226a583812eaaac079812
Serco Honan <ashyir@yahoo.com>
2024-08-08 11:54:08 +0700

    Modify Week 06.

--- Week 06/index.js
+++ Week 06/index.js
@@ -1,63 +1,63 @@
-document.getElementById('frm-add-product').onsubmit = addProduct
+const frmAddProduct = document.getElementById('frm-add-product')
+
+window.onload = loadProductFromDB
+frmAddProduct.onsubmit = addProduct
 
 function addProduct(e) {
     e.preventDefault();
 
     let product = {}
+
     product.name = document.getElementById('product-name').value
     product.price = parseFloat(document.getElementById('product-price').value)
     product.quantity = parseInt(document.getElementById('product-quantity').value)
-    product.amount = product.price * product.quantity
 
-    const table = document.getElementById('table-product').getElementsByTagName('tbody')[0]
+    if (product.name === '' || isNaN(product.price) || isNaN(product.quantity))
+        return
 
-    const new_row = table.insertRow() // <tr></tr>
-    const col_1 = new_row.insertCell(0) // <tr><td></td></tr>
-    const col_2 = new_row.insertCell(1) // <tr><td></td><td></td></tr>
-    const col_3 = new_row.insertCell(2) // <tr><td></td><td></td><td></td></tr>
-    const col_4 = new_row.insertCell(3) // <tr><td></td><td></td><td></td><td></td></tr>
+    loadProductIntoTable(product)
+    saveProductToDB(product)
+    showMessage()
 
-    col_1.innerHTML = product.name // <tr><td>name</td><td></td><td></td><td></td></tr>
-    col_2.innerHTML = product.price
-    col_3.innerHTML = product.quantity
-    col_4.innerHTML = product.amount
-
-    save_product(product)
-    show_message()
+    frmAddProduct.reset()
 }
 
-function show_message() {
+function showMessage() {
     const message = document.getElementById('message')
+
     message.style.display = 'block'
 
-    setTimeout(() => {
-        message.style.display = 'none'
-    }, 2000)
+    setTimeout(() => message.style.display = 'none', 2000)
 }
 
-function save_product(product) {
+function loadProductIntoTable(product) {
+    const table = document.getElementById('table-product').getElementsByTagName('tbody')[0]
+
+    const newRow = table.insertRow()   // <tr></tr>
+
+    const col1 = newRow.insertCell(0) // <tr><td></td></tr>
+    const col2 = newRow.insertCell(1) // <tr><td></td><td></td></tr>
+    const col3 = newRow.insertCell(2) // <tr><td></td><td></td><td></td></tr>
+    const col4 = newRow.insertCell(3) // <tr><td></td><td></td><td></td><td></td></tr>
+
+    const amount = product.price * product.quantity
+
+    col1.innerHTML = product.name      // <tr><td>name</td><td></td><td></td><td></td></tr>
+    col2.innerHTML = product.price     // <tr><td>name</td><td>price</td><td></td><td></td></tr>
+    col3.innerHTML = product.quantity  // <tr><td>name</td><td>price</td><td>quantity</td><td></td></tr>
+    col4.innerHTML = amount            // <tr><td>name</td><td>price</td><td>quantity</td><td>amount</td></tr>
+}
+
+function saveProductToDB(product) {
     let products = JSON.parse(localStorage.getItem('products')) || []
+
     products.push(product)
+
     localStorage.setItem('products', JSON.stringify(products))
 }
 
-function load_product() {
-    let products = JSON.parse(localStorage.getItem('products')) || []
+function loadProductFromDB() {
+    const products = JSON.parse(localStorage.getItem('products')) || []
 
-    const table = document.getElementById('table-product').getElementsByTagName('tbody')[0]
-
-    for (const product of products) {
-        const new_row = table.insertRow() // <tr></tr>
-        const col_1 = new_row.insertCell(0) // <tr><td></td></tr>
-        const col_2 = new_row.insertCell(1) // <tr><td></td><td></td></tr>
-        const col_3 = new_row.insertCell(2) // <tr><td></td><td></td><td></td></tr>
-        const col_4 = new_row.insertCell(3) // <tr><td></td><td></td><td></td><td></td></tr>
-
-        col_1.innerHTML = product.name // <tr><td>name</td><td></td><td></td><td></td></tr>
-        col_2.innerHTML = product.price
-        col_3.innerHTML = product.quantity
-        col_4.innerHTML = product.amount
-    }
+    products.forEach(product => loadProductIntoTable(product))
 }
-
-window.onload = load_product
